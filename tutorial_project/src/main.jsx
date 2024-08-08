e363a4cb496b556a7af676038951ab50ae643738
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-08 14:07:42 +0400

    создал компонент для ошибок на странице

--- tutorial_project/src/main.jsx
+++ tutorial_project/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 import Root from "./routes/root";
 import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 createRoot(document.getElementById("root")).render(
