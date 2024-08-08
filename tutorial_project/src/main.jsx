2bc36e56d20a7f98916134991c278deab0a5a996
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-08 14:12:55 +0400

    добавил новый маршут с помощью компонента  contact.jsx

--- tutorial_project/src/main.jsx
+++ tutorial_project/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Root from "./routes/root";
+import Contact from "./routes/contact";
 import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 createRoot(document.getElementById("root")).render(
