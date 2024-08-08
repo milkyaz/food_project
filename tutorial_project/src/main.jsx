deae5add5663c9bf3d6b169c2a2c7e1d5cb773bd
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-08 14:04:32 +0400

    базовые файлы для начала

--- tutorial_project/src/main.jsx
+++ tutorial_project/src/main.jsx
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
 import "./index.css";
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-
   </StrictMode>
 );
