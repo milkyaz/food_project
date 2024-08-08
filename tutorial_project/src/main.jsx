3f35eca4052ec95316e153dee823dabb87a06a44
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-08 14:31:11 +0400

    настроил загрузчик на маршруте

--- tutorial_project/src/main.jsx
+++ tutorial_project/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import Contact from "./routes/contact";
 import "./index.css";
 const router = createBrowserRouter([
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
