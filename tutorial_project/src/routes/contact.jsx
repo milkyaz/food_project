502bc12990d63a3d6a481763bcce2579cc345dcf
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-10 00:25:08 +0400

    Преобразование кода в main на JSX c помощью createRoutesFromElements

--- tutorial_project/src/routes/contact.jsx
+++ tutorial_project/src/routes/contact.jsx
@@ -10,6 +10,12 @@ export async function action({ request, params }) {
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
@@ -72,7 +78,9 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   const fetcher = useFetcher();
-  const favorite = contact.favorite;
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
   return (
     <fetcher.Form method="post">
       <button
